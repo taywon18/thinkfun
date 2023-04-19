68975c721a939e11c0d495966adf7f78e3cdf823
Taywon <thespartan18@hotmail.fr>
2023-04-19 20:54:49 +0200

    added base of datastore

---
+++ ThinkFun.Server/DataStore.cs
@@ -0,0 +1,14 @@
+﻿namespace ThinkFun.Server;
+using MongoDB.Driver;
+
+
+public class DataStore
+{
+    public static DataStore Instance { get;  } = new DataStore();
+
+    public DataStore()
+    {
+
+    }
+
+}
---
+++ ThinkFun.Server/Sources/ThemeParksNodeApi/ThemeParksNodeApiSource.cs
@@ -0,0 +1,28 @@
+﻿namespace ThinkFun.Server.Sources.ThemeParksNodeApi;
+
+public class ThemeParksNodeApiSource
+    : IDataSource
+{
+    public const string ExternalIdKey = "tpna";
+    public const string CommonIdKey = "common";
+    public bool UseParkPositionForDestinationIfUnknown = true;
+    HttpClient Client;
+
+    public ThemeParksNodeApiSource()
+    {
+        Client = new()
+        {
+            BaseAddress = new Uri("https://tp.arendz.nl/")
+        };
+    }
+
+    public override Task UpdateLiveData(DataCollection collection, CancellationToken t)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task UpdateStaticData(DataCollection collection, CancellationToken t)
+    {
+        throw new NotImplementedException();
+    }
+}
