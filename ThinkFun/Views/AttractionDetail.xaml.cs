9cb6d8bb0bc41b4d49c62eab4397e4fa00fa6458
taywon <thespartan18@hotmail.fr>
2024-01-20 12:35:43 +0100

    worked on shell.titleview

--- ThinkFun/Views/AttractionDetail.xaml.cs
+++ ThinkFun/Views/AttractionDetail.xaml.cs
@@ -26,23 +26,7 @@ public partial class AttractionDetail
 		}
 	}
 
-    public ISeries[] Series { get; set; } =
-    {
-        /*new ColumnSeries<int>
-        {
-            Values = new[] { 6, 3, 5, 7, 3, 4, 6, 3 },
-            Stroke = null,
-            MaxBarWidth = double.MaxValue,
-            IgnoresBarPosition = true
-        },
-        new ColumnSeries<int>
-        {
-            Values = new[] { 2, 4, 8, 9, 5, 2, 4, 7 },
-            Stroke = null,
-            MaxBarWidth = 30,
-            IgnoresBarPosition = true
-        }*/
-    };
+    public ISeries[] Series { get; set; } = {};
 
 
 
